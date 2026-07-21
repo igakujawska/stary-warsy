68d8d5c8a3acc07136b478ed40607952f4dfe095
iga <igakuj1@gmail.com>
2026-07-21 14:22:58 +0200

    dodawanie zmian w services i config

--- starwarsy/src/app/core/services/star-wars-api.ts
+++ starwarsy/src/app/core/services/star-wars-api.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Service } from '@angular/core';
-
+  const SW_API = 'https://swapi.dev/api/';
 @Service()
-export class StarWarsApi {}
+export class StarWarsApi {
+  
+    private http = inject(HttpClient);
+
+}
